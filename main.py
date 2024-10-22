0b06898adbe9c25b9bebdd7a9629b15f5d4019a9
HappyHarry1130 <164897239+HappyHarry1130@users.noreply.github.com>
2024-10-22 11:29:37 -0700

    Update

--- main.py
+++ main.py
@@ -12,30 +12,43 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import random
 import urllib.parse
-import re
 from datetime import datetime
 import json
-import os
 from bs4 import BeautifulSoup
-from utilies import Extact_Email, write_to_google_sheet_3
+from utilies import Extact_Email, write_to_google_sheet_3, Ckeck_booking
 import sys
+from playwright.sync_api import sync_playwright
 def get_data_from_url(url, driver):
     print(url)
+
     try:
         driver.get(url)
         soup = BeautifulSoup(driver.page_source, 'html.parser')
-        booking_keywords = ["book online", "book appointment"]
-        content_text = soup.get_text().lower()
-        info =Extact_Email(content_text)
+        element_text = soup.find('body').get_text().lower()
+        a_text =[]
+        Booking_Keywords_Found_a = soup.find_all('a')
+        for a_element in Booking_Keywords_Found_a:
+            a_text.append(a_element.get_text())
+            
+
+        info =Extact_Email(element_text)
         info = json.loads(info)
         Address = info.get("Address")
         Emails = info.get("Emails")
-        Phonenumbers = info.get("Phonenumbers")
-        Booking_Keywords_Found = info.get("Booking Keywords Found")
+        Phonenumbers = info.get("Phonenumbers")        
+        XPaths = info.get("XPaths")
+        link = info.get("href")
+
+        booking_info = Ckeck_booking(a_text)
+        booking_info = json.loads(booking_info)
+        Booking_Keywords_Found = booking_info.get("Booking Keywords Found")
         print("Address:", Address)
         print("Emails:", Emails)
         print("Phonenumbers:", Phonenumbers)
         print("Booking Keywords Found:", Booking_Keywords_Found)
+        
+
+
         if len(Emails) >= 1:
             email = Emails[0]
         else: email = ''
@@ -57,31 +70,24 @@ def main():
     search_query = input("Enter your search query: ")
     encoded_query = urllib.parse.quote(search_query)
 
-    # chrome_options.add_argument("--headless")
+
     chrome_options = Options()
-    # chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--window-size=1280,800")
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-
-    # Install the Chrome driver
-    # proxy = 'socks5://14ae1ada67095:8f3793607c@139.171.124.133:12324'
-    # chrome_options.add_argument(f'--proxy-server={proxy}')
-
     driver.maximize_window()
     
-    # search_query = 'intext:"call to schedule" "contact us" "bridal" -site:healthcare.com'
     driver.get(f"https://www.google.com/search?q={encoded_query}")
     time.sleep(random.uniform(20,30))    
     urls = []
+  
     while True:
         links = driver.find_elements(By.XPATH, "//div[@class='yuRUbf']//a")
         urls.extend([link.get_attribute('href') for link in links])
-        # urls = [link.get_attribute('href') for link in links]
+
         print(len(urls))
         # for url in urls:
         #     get_data_from_url(url, driver=driver)            
@@ -97,17 +103,18 @@ def main():
 
     for index, url in enumerate( urls, start=1):
         percent = (index / len(urls)) * 100
-        bar_length = 50  # Length of the progress bar
+        bar_length = 50  
         filled_length = int(bar_length * index // len(urls))
         bar = '>' * filled_length + '-' * (bar_length - filled_length)
         sys.stdout.write(f"\rProcessing link {index}/{len(urls)}: [{bar}] {percent:.2f}%")
         sys.stdout.flush()
+        print(" ")
         try: 
             get_data_from_url(url, driver=driver)
             time.sleep(random.uniform(2,3))
         except TimeoutError:
             print (' Timeout Error')
         except: 
-            print('error') 
+            print('error_h') 
 
 main()
